73f70a48f7ab0010191d63ab7dbbd54922865281
Andre Hohenstein <68845584+AndreHohenstein@users.noreply.github.com>
2024-02-22 15:49:23 +0100

    Update Prepare-RootCA.ps1

--- PKI/Root-CA/Prepare-RootCA.ps1
+++ PKI/Root-CA/Prepare-RootCA.ps1
@@ -1,8 +1,10 @@
 # Erstellt einen neuen lokalen Benutzer und fuegt ihn zur Gruppe der Administratoren hinzu.
 # Alle weiteren Schritte bitte mit dem neuen Account durchfuehren.
-$Password = Read-Host -Prompt "Enter your Password" -AsSecureString
-New-LocalUser "Andre" -Password $Password -FullName "Andre Hohenstein" -Description "new loacal Admin" -PasswordNeverExpires
-Add-LocalGroupMember -Group "Administratoren" -Member "Andre"
+$Password  = Read-Host -Prompt "Enter your Password" -AsSecureString
+$UserName  = Read-Host -Prompt "Enter your Login Name"
+$FullNmame = Read-Host -Prompt "Enter yout FullName"
+New-LocalUser $UserName -Password $Password -FullName $FullNmame -Description "new loacal Admin" -PasswordNeverExpires
+Add-LocalGroupMember -Group "Administratoren" -Member $UserName
 
 Start-Sleep -Seconds 1
 
@@ -10,6 +12,6 @@ $NIC = (Get-NetAdapter | Where-Object {$_.Status -eq "Up"}).InterfaceAlias
  New-NetIPAddress -InterfaceAlias $Nic `
                   -IPAddress 192.168.1.99 `
                   -PrefixLength 24 `
-                  -DefaultGateway 192.168.1.1
-Set-DnsClientServerAddress -InterfaceAlias $Nic -ServerAddresses 192.168.1.101
+                  -DefaultGateway 192.168.1.1 | Out-Null
+Set-DnsClientServerAddress -InterfaceAlias $Nic -ServerAddresses 192.168.1.101 | Out-Null
 Rename-Computer -NewName RootCA -Restart
